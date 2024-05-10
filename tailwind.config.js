b5c43051d5b6a7c387ea53c7492edbff554181af
Gary11777 <iharushakou@gmail.com>
2024-05-10 12:46:16 +0300

    4 Make a Pretty Layout Using Tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.export = {
+  content: [
+      "../resources/views/**/*.{html,js,php}",
+      "../routes/**/*.{html,js,php}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
