0d01b06ae6be8d0a1dfd5a0475c92ea44b277d0d
Gary11777 <iharushakou@gmail.com>
2024-06-02 10:54:43 +0300

    jobs.blade changing

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -3,13 +3,14 @@
         Job Listings
     </x-slot:heading>
 
-    <ul>
+    <div class="space-y-4">
         @foreach ($jobs as $job)
-            <li>
-                <a href="/jobs/{{ $job['id'] }}" class="font-medium text-blue-600 underline dark:text-blue-500 hover:no-underline">
-                    <strong>{{ $job['title'] }}:</strong> Pays {{ $job['salary'] }} per year.
+                <a href="/jobs/{{ $job['id'] }}" class="block px-4 py-6 border border-gray-500 rounded-lg">
+                    <div>{{ $job->employer->name }}</div>
+                    <div>
+                        <strong>{{ $job['title'] }}:</strong> Pays {{ $job['salary'] }} per year.
+                    </div>
                 </a>
-            </li>
         @endforeach
-    </ul>
+    </div>
 </x-layout>
