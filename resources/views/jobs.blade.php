0c8734da728e314e35fdb1f0e64a38e77575e024
Gary11777 <iharushakou@gmail.com>
2024-06-02 11:44:29 +0300

    13 Eager Loading and the N+1 Problem

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -6,7 +6,7 @@
     <div class="space-y-4">
         @foreach ($jobs as $job)
                 <a href="/jobs/{{ $job['id'] }}" class="block px-4 py-6 border border-gray-500 rounded-lg">
-                    <div>{{ $job->employer->name }}</div>
+                    <div class="font-bold text-blue-500">{{ $job->employer->name }}</div>
                     <div>
                         <strong>{{ $job['title'] }}:</strong> Pays {{ $job['salary'] }} per year.
                     </div>
